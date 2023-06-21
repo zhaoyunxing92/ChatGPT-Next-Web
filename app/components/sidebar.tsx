c5470975f3b09a40d26b25508af482ae18268f4c
zhaoyunxing <zhaoyunxing@apache.org>
2023-06-21 16:58:54 +0800

    删除配置模块

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -128,22 +128,22 @@ export function SideBar(props: { className?: string }) {
         </div>
       </div>
 
-      <div className={styles["sidebar-header-bar"]}>
-        <IconButton
-          icon={<MaskIcon />}
-          text={shouldNarrow ? undefined : Locale.Mask.Name}
-          className={styles["sidebar-bar-button"]}
-          onClick={() => navigate(Path.NewChat, { state: { fromHome: true } })}
-          shadow
-        />
-        <IconButton
-          icon={<PluginIcon />}
-          text={shouldNarrow ? undefined : Locale.Plugin.Name}
-          className={styles["sidebar-bar-button"]}
-          onClick={() => showToast(Locale.WIP)}
-          shadow
-        />
-      </div>
+      {/*<div className={styles["sidebar-header-bar"]}>*/}
+      {/*  <IconButton*/}
+      {/*    icon={<MaskIcon />}*/}
+      {/*    text={shouldNarrow ? undefined : Locale.Mask.Name}*/}
+      {/*    className={styles["sidebar-bar-button"]}*/}
+      {/*    onClick={() => navigate(Path.NewChat, { state: { fromHome: true } })}*/}
+      {/*    shadow*/}
+      {/*  />*/}
+      {/*  <IconButton*/}
+      {/*    icon={<PluginIcon />}*/}
+      {/*    text={shouldNarrow ? undefined : Locale.Plugin.Name}*/}
+      {/*    className={styles["sidebar-bar-button"]}*/}
+      {/*    onClick={() => showToast(Locale.WIP)}*/}
+      {/*    shadow*/}
+      {/*  />*/}
+      {/*</div>*/}
 
       <div
         className={styles["sidebar-body"]}
